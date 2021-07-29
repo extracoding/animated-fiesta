dc183e3d34b347ceacb6c39d1bcc59961abde44d
Hassan <hassan@extracoding.com>
2021-07-29 16:39:17 +0500

    Gulp file updated

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,19 @@
 const gulp = require('gulp');
+const fs = require('fs');
+
+gulp.task( 'create-package-dir', () => {
+    // Create a version directory
+    //// if already exists then delete and create it
+    // Write a changelog file
+
+    return new Promise( resolve => {
+        if ( fs.existsSync('./final-packages/test-package') ) {
+            throw new Error("The package branch name is already exists test-package")
+        }
+
+        fs.mkdirSync( './final-packages/test-package' );
+        resolve()
+    })
+})
+
+gulp.task( 'default', gulp.series('create-package-dir' ) )
