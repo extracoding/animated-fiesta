d67c731258ec9ff7a895a3d42574a9a232b9bf03
Hassan <hassan@extracoding.com>
2021-07-29 16:42:02 +0500

    gulp file empty task added

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,11 @@
 const gulp = require('gulp');
 const fs = require('fs');
 
-gulp.task( 'create-package-dir', () => {
+gulp.task( 'create-package-dir', ( cb ) => {
     // Create a version directory
     //// if already exists then delete and create it
     // Write a changelog file
-
-    return new Promise( resolve => {
-        if ( fs.existsSync('./final-packages/test-package') ) {
-            throw new Error("The package branch name is already exists test-package")
-        }
-
-        fs.mkdirSync( './final-packages/test-package' );
-        resolve()
-    })
+    cb()
 })
 
 gulp.task( 'default', gulp.series('create-package-dir' ) )
