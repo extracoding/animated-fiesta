e775bc1e6ea4122b577ac200073b028f7001617e
Hassan <hassan@extracoding.com>
2021-07-29 16:44:58 +0500

    directories added

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,15 @@ gulp.task( 'create-package-dir', ( cb ) => {
     // Create a version directory
     //// if already exists then delete and create it
     // Write a changelog file
-    cb()
+
+    return new Promise( resolve => {
+        if ( fs.existsSync('./final-packages/test-package') ) {
+            throw new Error("The package branch name is already exists test-package")
+        }
+
+        fs.mkdirSync( './final-packages/test-package' );
+        resolve()
+    })
 })
 
 gulp.task( 'default', gulp.series('create-package-dir' ) )
